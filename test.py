a0b00f1429edf4972a14e11ade16bfc1a8d2b126
Mohamad Nematizadeh <89206040+MohamadNematizadeh@users.noreply.github.com>
2023-08-13 15:54:45 +0330

    test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+f= 0
+while f <3:
+    pasword = int(input(":"))
+    pasword1= str(pasword)
+    if len(pasword1) ==4 and int :
+        pass_asly = 1386
+        if pasword == 1386:
+            print(",,,,") 
+            break
+
+        elif pasword == 8665 or float or str:
+            break    
