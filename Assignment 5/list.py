2281fa78f17e0588cc4570f07f316b40ea0934dd
Mohamad Nematizadeh <89206040+MohamadNematizadeh@users.noreply.github.com>
2023-08-15 22:26:12 +0330

    Add files via upload

---
+++ Assignment 5/list.py
@@ -0,0 +1,16 @@
+a = ['M',4,90,7,[80,7,'Q'],55,'N']
+z = ['HH','MM']
+
+print('A =', a)
+print('Z =', z)
+
+print (a[3])
+print (a[4][1])
+print (a[-1])
+print (a * 2)
+print (a[:2])
+print (a[:5:2])
+print (a + z)
+print (a[::-2])
+print (a[5:8:3])
+print (a + [555])
---
+++ Assignment 5/meth.py
@@ -0,0 +1,18 @@
+a = str(input(' your enter str:'))
+
+#   afzayesh tol reshte va gharar dadanesh dar vasat
+b = a.center(19)
+print('center :',b)
+
+#   tamom shodan reshte ba karakter khas 
+
+c = a.endswith('d')
+print('endswith :',c)
+
+#   chek kardan in ke tamam reshte fasele hast
+d = a.isspace()
+print('isspace :',d)
+
+#    tashkhis horof bodan reshte
+s = a.isalpha()
+print('isalpha :',s)
