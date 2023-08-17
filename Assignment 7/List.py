f8186f05ecfa74d18919cf48775fe5a93257c854
Mohamad Nematizadeh <89206040+MohamadNematizadeh@users.noreply.github.com>
2023-08-17 22:56:09 +0330

    Add files via upload

---
+++ Assignment 7/List.py
@@ -0,0 +1,9 @@
+list_One = ["py","i"," ","popul","lang"]
+list_two = ["thon","s","a","ar","uage."]
+list_Three = []
+list_Three.append(list_One[0] + list_two[0])
+list_Three.append(list_One[1] + list_two[1])
+list_Three.append(list_One[2] + list_two[2])
+list_Three.append(list_One[3] + list_two[3])
+list_Three.append(list_One[4] + list_two[4])
+print(list_Three)
