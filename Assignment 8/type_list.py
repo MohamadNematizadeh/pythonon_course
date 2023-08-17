0e905d9bbfac92c0f3f625d644a40eb1b44562a0
Mohamad Nematizadeh <89206040+MohamadNematizadeh@users.noreply.github.com>
2023-08-17 22:47:38 +0330

    Add files via upload

---
+++ Assignment 8/type_list.py
@@ -0,0 +1,8 @@
+lists = ['Mohmmad',1,['MVM','BNS'],'Ali']
+lists[0] = 'Sader'
+print(lists)
+set1 = print({'Blue','Brown','Red','Cyan','Aquamarine','Blue'})
+tuple = print(('Mohmmad',1,['MVM','BNS'],'Ali'))
+dict = {"famle" : "Mohmmmad", "car" : "MVM"}
+dict["agi"] = "15"
+print(dict)
